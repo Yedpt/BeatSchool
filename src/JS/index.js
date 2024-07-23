a68c389350e3cf34296564a3e0861e4bfe70b39b
Yeder <ypimentel.tapia@gmail.com>
2024-07-23 20:43:52 +0200

    dejando listo el nav en contact

--- src/JS/index.js
+++ src/JS/index.js
@@ -23,51 +23,51 @@ document.querySelectorAll('.nav-link').forEach(link => {
 });
 // slider 
 
-const btnLeft = document.querySelector(".btn-left") /* creamos variables (constantes) y usamos el queryselector para jugar con los elementos del DOM */
-const btnRight = document.querySelector(".btn-right")
-const slider = document.querySelector("#slider")
-const sliderSection = document.querySelectorAll(".slider-section") /* aqui tenemos 4 elementos con la clase "slider-section entonces usamos queryselectorALl para selec" */
+// const btnLeft = document.querySelector(".btn-left") /* creamos variables (constantes) y usamos el queryselector para jugar con los elementos del DOM */
+// const btnRight = document.querySelector(".btn-right")
+// const slider = document.querySelector("#slider")
+// const sliderSection = document.querySelectorAll(".slider-section") /* aqui tenemos 4 elementos con la clase "slider-section entonces usamos queryselectorALl para selec" */
 
-btnLeft.addEventListener("click" , e => moveToLeft()) /* añado eventos de mover a la izquierda y derecha  */
-btnRight.addEventListener("click" , e => moveToRight())
+// btnLeft.addEventListener("click" , e => moveToLeft()) /* añado eventos de mover a la izquierda y derecha  */
+// btnRight.addEventListener("click" , e => moveToRight())
 
-setInterval(() =>{
-    moveToRight()
-}, 3000)
+// setInterval(() =>{
+//     moveToRight()
+// }, 3000)
 
-let operacion = 0
-let widthVid = 100 / sliderSection.length
-let counter = 0
+// let operacion = 0
+// let widthVid = 100 / sliderSection.length
+// let counter = 0
 
-function moveToRight () {
-    if(counter >= sliderSection.length - 1){
-        counter = 0
-        operacion = 0
-        slider.style.transform = `translate(-${operacion}%)`
-        slider.style.transition = "none"
-        return
-    }
-        counter++
-         operacion = operacion + widthVid
-        slider.style.transform = `translate(-${operacion}%)`
-        slider.style.transition = "all ease .6s"
+// function moveToRight () {
+//     if(counter >= sliderSection.length - 1){
+//         counter = 0
+//         operacion = 0
+//         slider.style.transform = `translate(-${operacion}%)`
+//         slider.style.transition = "none"
+//         return
+//     }
+//         counter++
+//          operacion = operacion + widthVid
+//         slider.style.transform = `translate(-${operacion}%)`
+//         slider.style.transition = "all ease .6s"
     
     
-}
+// }
 
-function moveToLeft () {
-    counter--
-    if (counter< 0) {
-        counter = sliderSection.length-1
-        operacion = widthVid * (sliderSection.length -1)
-        slider.style.transform = `translate(-${operacion}%)`
-        slider.style.transition = "none"
-        return
-    }
-        operacion = operacion - widthVid
-        slider.style.transform = `translate(-${operacion}%)`
-        slider.style.transition = "all ease .6s"
+// function moveToLeft () {
+//     counter--
+//     if (counter< 0) {
+//         counter = sliderSection.length-1
+//         operacion = widthVid * (sliderSection.length -1)
+//         slider.style.transform = `translate(-${operacion}%)`
+//         slider.style.transition = "none"
+//         return
+//     }
+//         operacion = operacion - widthVid
+//         slider.style.transform = `translate(-${operacion}%)`
+//         slider.style.transition = "all ease .6s"
     
   
-}
+// }
 
