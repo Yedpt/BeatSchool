8bd0b1cb28550c0059c87474b8428006c5ff3970
Yeder <ypimentel.tapia@gmail.com>
2024-07-31 20:40:57 +0200

    beatSchool terminada

--- src/JS/index.js
+++ src/JS/index.js
@@ -71,3 +71,59 @@ function moveToLeft () {
     
   
 }
+
+// la parte de compartir 
+
+document.getElementById('share-icon').addEventListener('click', function () {
+    console.log('Icono clicado')
+    document.getElementById('share-popup').style.display = 'block'
+})
+
+document.addEventListener('click', function (event) {
+    let popup = document.getElementById('share-popup')
+    if (!popup.contains(event.target) && event.target.id !== 'share-icon') {
+        console.log('Cerrando popup')
+        popup.style.display = 'none'
+    }
+})
+
+document.querySelectorAll('.share-button').forEach((button) => {
+    button.addEventListener('click', function () {
+        const platform = this.getAttribute('data-platform')
+        const url = window.location.href
+        let shareUrl = ''
+
+        const shareUrlBase = 'https://www.BeatSchool.com'
+
+        switch (platform) {
+            case 'facebook':
+                shareUrl = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(
+                    shareUrlBase
+                )}`
+                break
+            case 'twitter':
+                shareUrl = `https://twitter.com/intent/tweet?url=${encodeURIComponent(
+                    shareUrlBase
+                )}`
+                break
+            case 'whatsapp':
+                shareUrl = `https://api.whatsapp.com/send?text=${encodeURIComponent(
+                    shareUrlBase
+                )}`
+                break
+        }
+
+        console.log(`Abriendo ventana de compartir: ${shareUrl}`)
+        window.open(shareUrl, 'popup', 'width=600,height=400')
+    })
+})
+
+document.getElementById('copy-link').addEventListener('click', function () {
+    const url = 'www.beatschool.com'
+    navigator.clipboard.writeText(url).then(() => {
+        const confirmation = document.getElementById('copy-confirmation')
+        confirmation.classList.remove('hidden')
+        setTimeout(() => confirmation.classList.add('hidden'), 4000)
+        console.log('Enlace copiado')
+    })
+})
