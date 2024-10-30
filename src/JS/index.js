db46faa2e2b6973be0420a2f06e636023b659c38
jruizndev <jorsqn@gmail.com>
2024-10-30 19:35:02 +0100

    solucionando commits

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,38 +1,46 @@
-// aqui voy hacer la funcion de abrir y cerrar el menu desplegable cuando sea mobile 
+// aqui voy hacer la funcion de abrir y cerrar el menu desplegable cuando sea mobile
 
 let menuVisible = false
 //funcion que oculta y muestra el menu
 function mostrarOcultarMenu() {
-    const nav = document.getElementById("nav");
+    const nav = document.getElementById('nav')
     if (menuVisible) {
-        nav.classList.remove("responsive");
-        menuVisible = false;
+        nav.classList.remove('responsive')
+        menuVisible = false
     } else {
-        nav.classList.add("responsive");
-        menuVisible = true;
+        nav.classList.add('responsive')
+        menuVisible = true
     }
 }
 
 // añado un evento a cada enlace para cerrar el menú al hacer clic
-document.querySelectorAll('.nav-link').forEach(link => {
-    link.addEventListener('click', function() {
-        const nav = document.getElementById("nav");
-        nav.classList.remove("responsive");
-        menuVisible = false;
-    });
-});
+document.querySelectorAll('.nav-link').forEach((link) => {
+    link.addEventListener('click', function () {
+        const nav = document.getElementById('nav')
+        nav.classList.remove('responsive')
+        menuVisible = false
+    })
+})
 
-// slider 
+// slider
 
-const btnLeft = document.querySelector(".btn-left") /* creamos variables (constantes) y usamos el queryselector para jugar con los elementos del DOM */
-const btnRight = document.querySelector(".btn-right")
-const slider = document.querySelector("#slider")
-const sliderSection = document.querySelectorAll(".slider-section") /* aqui tenemos 4 elementos con la clase "slider-section entonces usamos queryselectorALl para selec" */
+const btnLeft =
+    document.querySelector(
+        '.btn-left'
+    ) /* creamos variables (constantes) y usamos el queryselector para jugar con los elementos del DOM */
+const btnRight = document.querySelector('.btn-right')
+const slider = document.querySelector('#slider')
+const sliderSection =
+    document.querySelectorAll(
+        '.slider-section'
+    ) /* aqui tenemos 4 elementos con la clase "slider-section entonces usamos queryselectorALl para selec" */
 
-btnLeft.addEventListener("click" , e => moveToLeft()) /* añado eventos de mover a la izquierda y derecha  */
-btnRight.addEventListener("click" , e => moveToRight())
+btnLeft.addEventListener('click', (e) =>
+    moveToLeft()
+) /* añado eventos de mover a la izquierda y derecha  */
+btnRight.addEventListener('click', (e) => moveToRight())
 
-setInterval(() =>{
+setInterval(() => {
     moveToRight()
 }, 3000)
 
@@ -40,39 +48,35 @@ let operacion = 0
 let widthVid = 100 / sliderSection.length
 let counter = 0
 
-function moveToRight () {
-    if(counter >= sliderSection.length - 1){
+function moveToRight() {
+    if (counter >= sliderSection.length - 1) {
         counter = 0
         operacion = 0
         slider.style.transform = `translate(-${operacion}%)`
-        slider.style.transition = "none"
+        slider.style.transition = 'none'
         return
     }
-        counter++
-         operacion = operacion + widthVid
-        slider.style.transform = `translate(-${operacion}%)`
-        slider.style.transition = "all ease .6s"
-    
-    
+    counter++
+    operacion = operacion + widthVid
+    slider.style.transform = `translate(-${operacion}%)`
+    slider.style.transition = 'all ease .6s'
 }
 
-function moveToLeft () {
+function moveToLeft() {
     counter--
-    if (counter< 0) {
-        counter = sliderSection.length-1
-        operacion = widthVid * (sliderSection.length -1)
+    if (counter < 0) {
+        counter = sliderSection.length - 1
+        operacion = widthVid * (sliderSection.length - 1)
         slider.style.transform = `translate(-${operacion}%)`
-        slider.style.transition = "none"
+        slider.style.transition = 'none'
         return
     }
-        operacion = operacion - widthVid
-        slider.style.transform = `translate(-${operacion}%)`
-        slider.style.transition = "all ease .6s"
-    
-  
+    operacion = operacion - widthVid
+    slider.style.transform = `translate(-${operacion}%)`
+    slider.style.transition = 'all ease .6s'
 }
 
-// la parte de compartir 
+// la parte de compartir
 
 document.getElementById('share-icon').addEventListener('click', function () {
     console.log('Icono clicado')
@@ -90,7 +94,6 @@ document.addEventListener('click', function (event) {
 document.querySelectorAll('.share-button').forEach((button) => {
     button.addEventListener('click', function () {
         const platform = this.getAttribute('data-platform')
-        const url = window.location.href
         let shareUrl = ''
 
         const shareUrlBase = 'https://www.BeatSchool.com'
